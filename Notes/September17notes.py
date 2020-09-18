4a3c217750ec1c409eb0c7cb32f69c0d68750ca1
bellRNS <54106375+bellRNS@users.noreply.github>
2020-09-18 14:40:23 -0300

    adding assignment1

--- Notes/September17notes.py
+++ Notes/September17notes.py
@@ -1,12 +1,10 @@
 
-If I leave my house at 6:52 AM, and run 1 km at an easy pace (8:15min/km), then 3 km at a hard pace (7:12min/km) and 1 km at easy pace again, what time do I get home for breakfast?
-
-
-
-
-
-
-
+"""
+If I leave my house at 6:52 AM, and run 1 km at
+an easy pace (8:15min/km), then 3 km at a hard
+pace (7:12min/km) and 1 km at easy pace again,
+what time do I get home for breakfast?
+"""
 
 # Welcome! This is how we comment in Python!
 
@@ -16,7 +14,7 @@ and this can last a couple of lines"""
 # we will take our runner assignment, and make it into
 # a set of instructions
 
-initialTime = 6*60 + 4 # when i left the house
+initial_time = 6*60 + 4 # when i left the house
 
 # these tell me the speeds of runner
 slow_speed = 8 + (15/60)
@@ -32,7 +30,7 @@ fast_time = fast_distance*fast_speed
 
 #these tell me total time and time at home.
 total_time = slow_time + fast_time
-time_at_home = initialTime + total_time
+time_at_home = initial_time + total_time
 
 #output the actual answers!
 print('The total time elapsed was:', total_time)
