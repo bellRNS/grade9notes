a287ec7ae22f9f3bcd0e92ced6a04b7454cb1449
bellRNS <54106375+bellRNS@users.noreply.github>
2020-10-06 12:32:51 -0300

    updating If_answers.py

--- Exercises/If_Answers.py
+++ Exercises/If_Answers.py
@@ -4,3 +4,25 @@
 def tree():
     for i in range(1,8):
         print(i*'#')
+
+#level 4-5 Beans
+# Steps:
+# 1 read a string (store as variable)
+# 2 for each letter do this:
+# 3 if a letter == "B": do this (add to a variable)
+
+def beans():
+    x = str(input('What text should we use?'))
+    count = 0
+    for i in x:
+            if i == 'B':
+                count += 1
+    print('there are ', count, ' many Bs!')
+            
+    
+#level 6-7
+# if something is divisible with no remainder
+# modulo (%) division will return 0 
+
+
+
